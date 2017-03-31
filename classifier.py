471782427c4a213eec49dbf47c244ffe588c2017
ha-hao <haotian3gy@gmail.com>
2017-04-01 03:43:36 +0800

    c

--- classifier.py
+++ classifier.py
@@ -1,18 +1,40 @@
 import os
 # install tldextract
 
+
 class classifier:
     def __init__(self):
         self.checkoutDict = dict()
-        self.couponsSet = set()
+        self.couponSet = set()
         self.reviewSet = set()
         self.socialMediaSet = set()
 
+    def __init__(self, path):
+        self.checkoutDict = dict()
+        self.couponSet = set()
+        self.reviewSet = set()
+        self.socialMediaSet = set()
+
+        self.add_categories_from_folder(os.path.join(path, "DividedSiteToken"))
+        with open(os.path.join(path, "coupon.txt")) as f:
+            self.add_coupon_from_file(f)
+        f.close()
+        with open(os.path.join(path, "ReviewSiteDomain.txt")) as f:
+            self.add_review_from_file(f)
+        f.close()
+        with open(os.path.join(path, "SocialMediaDomain.txt")) as f:
+            self.add_socialmedia_from_file(f)
+        f.close()
+
     def add_categories_from_folder(self, path):
-        os.listdir(path)
+        for filename in os.listdir(path):
+            f = open(os.path.join(path, filename), "r")
+            with open(os.path.join(path, filename), "r") as f:
+                self.add_category_from_file(f)
+            f.close()
 
     def add_category_from_file(self, f):
-        category = file.names[:-9]
+        category = os.path.basename(f.name)[:-9]
         for line in f:
             key = line.split("\t")[0]
             value = line.split("\t")[1],line.split("\t")[2],category
@@ -20,45 +42,65 @@ class classifier:
 
     def add_coupon_from_file(self, f):
         for line in f:
-            self.couponSet.add(line)
+            if "\t" in line:
+                line = line.split("\t")[0]
+            if line[0] == ".":
+                line = line[1:]
+            self.couponSet.add(line.rstrip())
+
 
     def add_review_from_file(self, f):
         for line in f:
-            self.reviewSet.add(line)
+            if line[0] == ".":
+                line = line[1:]
+            self.reviewSet.add(line.rstrip())
 
     def add_socialmedia_from_file(self, f):
         for line in f:
-            self.socialMediaSet.add(line)
+            if line[0] == ".":
+                line = line[1:]
+            self.socialMediaSet.add(line.rstrip())
 
     def in_target_site(self, domain):
         return domain in self.checkoutDict
+
+    def in_relevant_site(self, domain):
+        return (domain in self.checkoutDict or domain in self.reviewSet or domain in self. socialMediaSet or
+            domain in self.couponSet)
+
 
     def get_category(self, domain):
         if self.in_target_site(domain):
             return self.checkoutDict[domain][2]
         else:
-            print("Key not found in category dict")
             return "Key Not Found"
 
     def get_checkoutURL(self, domain):
         if self.in_target_site(domain):
             return self.checkoutDict[domain][1]
         else:
-            print("Key not found in category dict")
             return "Key Not Found"
 
     def get_productURL(self, domain):
         if self.in_target_site(domain):
             return self.checkoutDict[domain][0]
         else:
-            print("Key not found in category dict")
             return "Key Not Found"
 
     def checkout_match(self, domain, url):
-        return self.get_checkoutURL(self, domain) == url
+        return self.get_checkoutURL( domain) == url
 
     def product_match(self, domain, url):
-        return self.get_productURL(self, domain) == url
+        return self.get_productURL(domain) == url
+
+    def is_coupon(self, domain):
+        return domain in self.couponSet
+
+    def is_review(self, domain):
+        return domain in self.reviewSet
+
+    def is_socialmedia(self, domain):
+        return domain in self.socialMediaSet
 
 
 
--- preProcessor.py
+++ preProcessor.py
@@ -2,6 +2,8 @@ import os
 import classifier
 from datetime import datetime, timedelta
 from tld import get_tld
+import glob
+
 
 class preProcessor:
     def __init__(self):
@@ -9,53 +11,64 @@ class preProcessor:
         self.start_date = ""
         self.end_date = ""
         self.data_dir = ""
+        self.tempdata_dir = ""
 
     def __init__(self, classifier, start_month, end_month, data_dir):
         self.classifier = classifier
-        self.start_date = start_month
-        self.end_date = end_month
+        self.start_date = datetime.strptime(start_month, '%Y%m%d')
+        self.end_date = datetime.strptime(end_month, '%Y%m%d')
         self.data_dir = data_dir
+        self.tempdata_dir = data_dir + "/temp_data"
+        if not os.path.exists(self.tempdata_dir):
+            os.makedirs(self.tempdata_dir)
 
     def split_files_by_hours(self):
         for single_date in daterange(self.start_date, self.end_date):
-            filepath = os.path.join(self.data_dir,  single_date.strftime("%Y%m%d"))
+            #filepath = os.path.join(self.data_dir,  single_date.strftime("%Y%m%d"))
+            filepath = os.path.join(os.path.join(self.data_dir,  single_date.strftime("%Y%m")),single_date.strftime("%Y%m%d"))
+            filepath += "_raw_qa_100K.bcp"
             with open(filepath, "r") as f:
-                self.split_file(self, f)
+                self.split_file(f)
             f.close()
-
-    def construct_instructions(self, n):
-        for single_date in daterange(self.start_date, self.end_date):
-            filepath = os.path.join(self.data_dir,  single_date.strftime("%Y%m%d"))
-            for i in range(23):
-                filepath += format(i, '02d')
-                with open(filepath, "r") as f:
-                    self.construct_active_map_instructions(self, f, n)
-                f.close()
 
     def split_file(self, f):
         #splitting, get useful domains, extract domains
-        filenpaths = []
-        for i in range(23):
-            filenpaths.append(os.path.join(self.data_dir, (f.names[:8] + format(i, '02d'))))
-
+        filepaths = []
+        for i in range(0,24):
+            filepaths.append(os.path.join(self.tempdata_dir, os.path.basename(f.name)[:9] + format(i, '02d')))
         files = []
+
         try:
-            for i in filenpaths:
+            for i in filepaths:
                 files.append(open(i, 'w'))
 
             for line in f:
                 linelist = line.split("\t")
-                domain = get_tld(linelist[0], fix_protocol=True)
+                try:
+                    linelist[0] = get_tld(linelist[0], fix_protocol=True)
+                except:
+                    linelist[0] = ""
                 hour = get_hour(int(linelist[6]))
-                if self.in_target_site(domain):
-                    files[hour].write(line)
+                if self.classifier.in_relevant_site(linelist[0]):
+                    files[hour].write("\t".join(linelist))
         finally:
             for i in files:
                 i.close()
 
-    def construct_active_map_instructions(self, f, n):
-        end_time_file_path = os.path.join(self.data_dir, f.names[:10] + "i")
-        start_time_file_path = os.path.join(self.data_dir, get_start_date(f.names[:10], n) +"i")
+    def construct_instructions(self, n):
+        for i in glob.glob(os.path.join(self.tempdata_dir,"*i.txt")):
+            os.remove(i)
+
+        for single_date in daterange(self.start_date, self.end_date):
+            for i in range(0,24):
+                path = os.path.join(self.tempdata_dir, single_date.strftime("%Y%m%d") + "_" + format(i, '02d'))
+                with open(path, "r") as f:
+                    self.construct_active_map_instruction(f, n)
+                f.close()
+
+    def construct_active_map_instruction(self, f, n):
+        end_time_file_path = os.path.join(self.tempdata_dir, os.path.basename(f.name)[:11] + "i.txt")
+        start_time_file_path = os.path.join(self.tempdata_dir, get_start_date(os.path.basename(f.name)[:11], n) +"i.txt")
         with open(end_time_file_path, write_append(end_time_file_path)) as fe, \
                 open(start_time_file_path, write_append(start_time_file_path)) as fs:
             for line in f:
@@ -63,6 +76,7 @@ class preProcessor:
                 if self.classifier.checkout_match(linelist[0],linelist[2]):
                     fe.write("-" + linelist[6] + "_" + linelist[0] + "\n")
                     fs.write(linelist[6] + "_" + linelist[0] + "\n")
+
         fe.close()
         fs.close()
 
@@ -80,11 +94,11 @@ def write_append(filepath):
 
 
 def get_start_date(endtime, n):
-    return (datetime.strptime(endtime, '%Y%m%d%H') - timedelta(days = n)).strftime('%Y%m%d%H')
+    return (datetime.strptime(endtime, '%Y%m%d_%H') - timedelta(days = n)).strftime('%Y%m%d_%H')
 
 
 def get_hour(ss2k):
-    return (ss2k/86400)/24
+    return (ss2k%86400)%24
 
 
 def get_ss2k(date_in_string):
